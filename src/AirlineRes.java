951a08ad5b7a12e4dd7229134a67a09c9f722cef
danderson100 <danderson7790@gmail.com>
2021-02-02 13:41:48 -0700

    added getters/setters/toString

--- src/AirlineRes.java
+++ src/AirlineRes.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 /**
  * AUTHOR: David Anderson
@@ -15,6 +14,56 @@ public class AirlineRes extends Reservation{
     private String airline;
     private Flight flight;
 
+    public AirlineRes(User user, String airline, Flight flight) {
+        this.user = user;
+        this.airline = airline;
+        this.flight = flight;
+    }
 
+    public User getUser() {
+        return user;
+    }
 
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public void setAirline(String airline) {
+        this.airline = airline;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        AirlineRes that = (AirlineRes) o;
+        return Objects.equals(user, that.user) && Objects.equals(airline, that.airline) && Objects.equals(flight, that.flight);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), user, airline, flight);
+    }
+
+    @Override
+    public String toString() {
+        return "AirlineRes{" +
+                "user=" + user +
+                ", airline='" + airline + '\'' +
+                ", flight=" + flight +
+                '}';
+    }
 }
